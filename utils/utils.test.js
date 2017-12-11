fcef11cad11c3adb3c1326fa747ab6d10b084161
Nadeem Ansari <s524968@mail.nwmissouri.edu>
2017-12-10 16:07:00 -0800

    Add ExpectJS assertion library and use methods.

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,15 +1,20 @@
+const expect = require('expect');
 const utils = require('./utils');
 
 it('should add two numbers', () => {
     var result = utils.add(33, 11);
 
-    if (result !== 44)
-        throw new Error(`Expected 44, but got ${result}.`);
+    expect(result).toBe(44).toBeA('number');
 });
 
 it('should square a number', () => {
     var result = utils.square(4);
 
-    if (result !== 16)
-        throw new Error(`Expected 16, but got ${result}`);
+    expect(result).toBe(16).toBeA('number');
+});
+
+it('should verify first and last names are set', () => {
+    var user = utils.setName({ firstName: '', lastName: ''}, 'Nadeem Ansari');
+
+    expect(user).toInclude({ firstName: 'Nadeem', lastName: 'Ansari'}).toBeA('object');
 });
