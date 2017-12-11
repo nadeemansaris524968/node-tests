1ae9fd1236f755a1871eb8ea856fa1e82e1a046a
Nadeem Ansari <s524968@mail.nwmissouri.edu>
2017-12-11 09:23:48 -0800

    Using rewire to spy on methods.

--- spies/app.test.js
+++ spies/app.test.js
@@ -1,10 +1,26 @@
 const expect = require('expect');
+const rewire = require('rewire');
+
+var app = rewire('./app');
 
 describe('App', () => {
 
+    var db = {
+        saveUser: expect.createSpy()
+    };
+    app.__set__('db', db);
+
     it('should call the spy method', () => {
         var spy = expect.createSpy();
-        spy('Andrew');
-        expect(spy).toHaveBeenCalled('Andrew');
+        spy('Andrew', 25);
+        expect(spy).toHaveBeenCalledWith('Andrew', 25);
+    });
+
+    it('should call saveUser with user object', () => {
+        var email = 'andrew@example.com';
+        var password = '123abc';
+
+        app.handleSignup(email, password);
+        expect(db.saveUser).toHaveBeenCalledWith({ email, password });
     });
 });
